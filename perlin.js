506fa3243c8925b9ccd4121f3f23b96b210562cb
David Furlong <dvfurlong@googlemail.com>
2016-12-28 17:15:29 +0100

    Fix syntax for constants
    Previous code used ES7 proposal, and is thus not yet supported. made constants global variables

--- perlin.js
+++ perlin.js
@@ -17,7 +17,9 @@
 
 class Grad {
   constructor(x,y,z){
-    this.x = x; this.y = y; this.z = z;
+    this.x = x;
+    this.y = y; 
+    this.z = z;
   }
 
   dot2(x,y){
@@ -28,6 +30,31 @@ class Grad {
     return this.x*x + this.y*y + this.z*z;
   }
 }
+
+const F2 = 0.5*(Math.sqrt(3)-1);
+const G2 = (3-Math.sqrt(3))/6;
+const F3 = 1/3;
+const G3 = 1/6;
+const grad3 = [new Grad(1,1,0),new Grad(-1,1,0),new Grad(1,-1,0),new Grad(-1,-1,0),
+               new Grad(1,0,1),new Grad(-1,0,1),new Grad(1,0,-1),new Grad(-1,0,-1),
+               new Grad(0,1,1),new Grad(0,-1,1),new Grad(0,1,-1),new Grad(0,-1,-1)];
+
+const p = [
+  151,160,137,91,90,15,
+  131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+  190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+  88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+  77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+  102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+  135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+  5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+  223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+  129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+  251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+  49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+  138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+];
+
 
 export default class Noise {
   constructor(seed = 0){
@@ -51,24 +78,24 @@ export default class Noise {
     for(var i = 0; i < 256; i++) {
       let v;
       if (i & 1) {
-        v = this.p[i] ^ (seed & 255);
+        v = p[i] ^ (seed & 255);
       }
       else {
-        v = this.p[i] ^ ((seed >> 8) & 255);
+        v = p[i] ^ ((seed >> 8) & 255);
       }
 
       this.perm[i] = this.perm[i + 256] = v;
-      this.gradP[i] = this.gradP[i + 256] = this.grad3[v % 12];
+      this.gradP[i] = this.gradP[i + 256] = grad3[v % 12];
     }
   }
 
   simplex2(xin, yin) {
     var n0, n1, n2; // Noise contributions from the three corners
     // Skew the input space to determine which simplex cell we're in
-    var s = (xin+yin)*this.F2; // Hairy factor for 2D
+    var s = (xin+yin)*F2; // Hairy factor for 2D
     var i = Math.floor(xin+s);
     var j = Math.floor(yin+s);
-    var t = (i+j)*this.G2;
+    var t = (i+j)*G2;
     var x0 = xin-i+t; // The x,y distances from the cell origin, unskewed.
     var y0 = yin-j+t;
     // For the 2D case, the simplex shape is an equilateral triangle.
@@ -83,10 +110,10 @@ export default class Noise {
     // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
     // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
     // c = (3-sqrt(3))/6
-    var x1 = x0 - i1 + this.G2; // Offsets for middle corner in (x,y) unskewed coords
-    var y1 = y0 - j1 + this.G2;
-    var x2 = x0 - 1 + 2 * this.G2; // Offsets for last corner in (x,y) unskewed coords
-    var y2 = y0 - 1 + 2 * this.G2;
+    var x1 = x0 - i1 + G2; // Offsets for middle corner in (x,y) unskewed coords
+    var y1 = y0 - j1 + G2;
+    var x2 = x0 - 1 + 2 * G2; // Offsets for last corner in (x,y) unskewed coords
+    var y2 = y0 - 1 + 2 * G2;
     // Work out the hashed gradient indices of the three simplex corners
     i &= 255;
     j &= 255;
@@ -127,12 +154,12 @@ export default class Noise {
     var n0, n1, n2, n3; // Noise contributions from the four corners
 
     // Skew the input space to determine which simplex cell we're in
-    var s = (xin+yin+zin) * this.F3; // Hairy factor for 2D
+    var s = (xin+yin+zin) * F3; // Hairy factor for 2D
     var i = Math.floor(xin+s);
     var j = Math.floor(yin+s);
     var k = Math.floor(zin+s);
 
-    var t = (i+j+k) * this.G3;
+    var t = (i+j+k) * G3;
     var x0 = xin-i+t; // The x,y distances from the cell origin, unskewed.
     var y0 = yin-j+t;
     var z0 = zin-k+t;
@@ -155,17 +182,17 @@ export default class Noise {
     // a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z), and
     // a step of (0,0,1) in (i,j,k) means a step of (-c,-c,1-c) in (x,y,z), where
     // c = 1/6.
-    var x1 = x0 - i1 + this.G3; // Offsets for second corner
-    var y1 = y0 - j1 + this.G3;
-    var z1 = z0 - k1 + this.G3;
+    var x1 = x0 - i1 + G3; // Offsets for second corner
+    var y1 = y0 - j1 + G3;
+    var z1 = z0 - k1 + G3;
 
-    var x2 = x0 - i2 + 2 * this.G3; // Offsets for third corner
-    var y2 = y0 - j2 + 2 * this.G3;
-    var z2 = z0 - k2 + 2 * this.G3;
+    var x2 = x0 - i2 + 2 * G3; // Offsets for third corner
+    var y2 = y0 - j2 + 2 * G3;
+    var z2 = z0 - k2 + 2 * G3;
 
-    var x3 = x0 - 1 + 3 * this.G3; // Offsets for fourth corner
-    var y3 = y0 - 1 + 3 * this.G3;
-    var z3 = z0 - 1 + 3 * this.G3;
+    var x3 = x0 - 1 + 3 * G3; // Offsets for fourth corner
+    var y3 = y0 - 1 + 3 * G3;
+    var z3 = z0 - 1 + 3 * G3;
 
     // Work out the hashed gradient indices of the four simplex corners
     i &= 255;
@@ -228,14 +255,14 @@ export default class Noise {
     var n10 = this.gradP[X+1+this.perm[Y]].dot2(x-1, y);
     var n11 = this.gradP[X+1+this.perm[Y+1]].dot2(x-1, y-1);
 
-    // Compute the this.fade curve value for x
-    var u = this.fade(x);
+    // Compute the this.constructor.fade curve value for x
+    var u = this.constructor.fade(x);
 
     // Interpolate the four results
-    return  this.lerp(
-              this.lerp(n00, n10, u),
-              this.lerp(n01, n11, u),
-              this.fade(y)
+    return  this.constructor.lerp(
+              this.constructor.lerp(n00, n10, u),
+              this.constructor.lerp(n01, n11, u),
+              this.constructor.fade(y)
             );
   }
 
@@ -257,21 +284,21 @@ export default class Noise {
     var n110 = this.gradP[X+1+this.perm[Y+1+this.perm[Z  ]]].dot3(x-1, y-1,   z);
     var n111 = this.gradP[X+1+this.perm[Y+1+this.perm[Z+1]]].dot3(x-1, y-1, z-1);
 
-    // Compute the this.fade curve value for x, y, z
-    var u = this.fade(x);
-    var v = this.fade(y);
-    var w = this.fade(z);
+    // Compute the this.constructor.fade curve value for x, y, z
+    var u = this.constructor.fade(x);
+    var v = this.constructor.fade(y);
+    var w = this.constructor.fade(z);
 
     // Interpolate
-    return  this.lerp(
-              this.lerp(
-                this.lerp(n000, n100, u),
-                this.lerp(n001, n101, u),
+    return  this.constructor.lerp(
+              this.constructor.lerp(
+                this.constructor.lerp(n000, n100, u),
+                this.constructor.lerp(n001, n101, u),
                 w
               ),
-              this.lerp(
-                this.lerp(n010, n110, u),
-                this.lerp(n011, n111, u),
+              this.constructor.lerp(
+                this.constructor.lerp(n010, n110, u),
+                this.constructor.lerp(n011, n111, u),
                 w
               ),
               v
@@ -285,30 +312,4 @@ export default class Noise {
   static lerp(a, b, t){
     return (1-t)*a + t*b;
   }
-
-  static F2 = 0.5*(Math.sqrt(3)-1);
-  static G2 = (3-Math.sqrt(3))/6;
-
-  static F3 = 1/3;
-  static G3 = 1/6;
-
-  static grad3 = [new Grad(1,1,0),new Grad(-1,1,0),new Grad(1,-1,0),new Grad(-1,-1,0),
-                  new Grad(1,0,1),new Grad(-1,0,1),new Grad(1,0,-1),new Grad(-1,0,-1),
-                  new Grad(0,1,1),new Grad(0,-1,1),new Grad(0,1,-1),new Grad(0,-1,-1)];
-
-  static p = [
-    151,160,137,91,90,15,
-    131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-    190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-    88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-    77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-    102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-    135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-    5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-    223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-    129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-    251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-    49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-    138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
-  ]
 }
